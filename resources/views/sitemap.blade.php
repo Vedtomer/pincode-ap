9737104ab4e91f0db77c596a932b2e062d15bbc0
Ved Tomer <ved.prakash@vidyamandir.com>
2022-08-20 16:59:36 +0530

    dfdf

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,10 +1,8 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @foreach ($pincode as $pincode)
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+ @foreach ($pincode as $pincode)
         <url>
             <loc>{{url($pincode['slug'])}}</loc>
             <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime($pincode['updated_at'])) }}</lastmod>
-            <changefreq>daily</changefreq>
             <priority>0.6</priority>
         </url>
     @endforeach
